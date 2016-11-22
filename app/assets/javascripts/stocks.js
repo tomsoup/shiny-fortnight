c4b4829f89b0057ab72bdcea57f78036424c4f66
Tom Ka Chun Cheung <tomhk1123@gmail.com>
2016-11-22 01:52:10 -0500

    Removed es6 for heroku

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -1,6 +1,6 @@
-let init_stock_lookup;
+var init_stock_lookup;
 
-init_stock_lookup = () => {
+init_stock_lookup = function(){
   $('#stock-lookup-form').on('ajax:before', function(event, data, status){
     show_spinner();
   })
