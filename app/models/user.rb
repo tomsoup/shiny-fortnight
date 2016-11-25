53e3f15296795b86002dec28dbdbb037ad16cf35
Tom Ka Chun Cheung <tomhk1123@gmail.com>
2016-11-25 01:00:50 -0500

    ajax search for friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,16 @@ class User < ActiveRecord::Base
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
   has_many :friends, through: :friendships
+  has_many :friendships
 
   def full_name
     # if no last or first name for user
-    #return Anonymous
-    return "#{first_name} #{last_name}".strip if (first_name || last_name)
-    "Anonymous"
+    # return Anonymous
+    return "#{first_name} #{last_name}".strip if first_name || last_name
+    'Anonymous'
   end
-  #check if over limit
+
+  # check if over limit
   def can_add_stock?(ticker_symbol)
     under_stock_limit? && !stock_already_added?(ticker_symbol)
   end
@@ -22,11 +24,54 @@ class User < ActiveRecord::Base
     (user_stocks.count < 10)
   end
 
-
   def stock_already_added?(ticker_symbol)
     stock = Stock.find_by_ticker(ticker_symbol)
-    #if not found, return false
+    # if not found, return false
     return false unless stock
     user_stocks.where(stock_id: stock.id).exists?
+  end
+
+  def not_friends_with?(friend_id)
+    # if less than one, that mean it did not show up at least once, thus they are not friend
+    friendships.where(friend_id: friend_id).count < 1
+  end
+
+  def except_current_user(users)
+    # looking for each element in the user object
+    # reject the one where the id of THAT user matches id of the current user
+    # self.id means the id of the caller
+    users.reject { |user| user.id == id }
+  end
+
+  # class level method for search
+  def self.search(param)
+    # return none if blank
+    return User.none if param.blank?
+
+    # ! to save
+    # strip to remove initial or tailing space
+    param.strip!
+    param.downcase!
+    # The methods are defined below
+    (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
+  end
+
+  # matches is what is doing the comparasion
+  def self.first_name_matches(param)
+    matches('first_name', param)
+  end
+
+  def self.last_name_matches(param)
+    matches('last_name', param)
+  end
+
+  def self.email_matches(param)
+    matches('email', param)
+  end
+
+  def self.matches(field_name, param)
+    # % means wild card
+    # % allow not exact match e.g john and joh
+    where("lower(#{field_name}) like ?", "%#{param}%")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "user/registrations" }
   resources :user_stocks, except: [:show, :edit, :update]
+  resources :users, only: [:show]
+  resources :friendships
   root 'home#index'
 
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
   get 'friends', to: "users#friends"
+  get 'search_friends', to: 'users#search'
+
+  #POST because we are creating
+  post 'add_friend', to: "users#add_friend"
 end
